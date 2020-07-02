6bb195e9fe723c102e85d9cb5ef753c195de03b4
Dmitry Shmatkov <dim4ik11061106@gmail.com>
2020-07-02 13:24:57 +0300

    Фиксы

--- static/js/leading-technologies.js
+++ static/js/leading-technologies.js
@@ -1,5 +1,21 @@
+function $_GET(key) {
+    var s = window.location.search;
+    s = s.match(new RegExp(key + '=([^&=]+)'));
+    return s ? s[1] : false;
+}
+
+function clickEmul(btn) {
+    $(btn).click();
+}
+
 $(document).ready(function () {
-    $('.contents__item').click(function (e) {
+    if ($_GET('opentech')) {
+        let i = $_GET('opentech');
+        let btn = $('.contents__item[data-linkToTech="' + i + '"]');
+        setTimeout(clickEmul, 20, btn);
+    }
+
+    $('.contents__item, .contents__title.audit-page').click(function (e) {
         e.preventDefault();
         $('.contents__item').removeClass('active');
         $(this).addClass('active');
@@ -7,5 +23,9 @@ $(document).ready(function () {
         $('.content-side.leading-technology').show();
         $('.content-side.leading-technology .technology-block').hide();
         $('.content-side.leading-technology .technology-block[data-technology="' + $(this).attr('data-linkToTech') + '"]').show();
+        let itemPos = $('.content-side.leading-technology .technology-block[data-technology="' + $(this).attr('data-linkToTech') + '"]').offset().top;
+        $("html, body").animate({
+            scrollTop: itemPos - 100
+        }, 300);
     });
 });
