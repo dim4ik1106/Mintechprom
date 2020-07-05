73c0a5364a633d8ff61cba799ca21e16f805b01a
Dmitry Shmatkov <dim4ik11061106@gmail.com>
2020-07-05 21:45:44 +0300

    photos

--- static/js/main.js
+++ static/js/main.js
@@ -1,58 +1,103 @@
-$(document).ready(function() {
-    if (c) {} else {
-        initTeaser();
-    }
-    function initTeaser() {
-        var iw = $('#teaserimg0' + ino).width();
-        var ih = $('#teaserimg0' + ino).height();
-        var aw = iw - stw;
-        var ah = ih - sth;
-        $('<span id="tit">' + $('.teaserimg0' + ino).html() + '</span>').appendTo('#startteaser').css({
-            'top': ytit + 'px'
-        }).fadeIn('fast').animate({
-            'top': '+=' + oytit + 'px'
-        }, (at - ottit)).fadeOut('slow');
-        $('#teaserimg0' + ino).animate({
-            'left': '-=' + aw + 'px',
-            'top': '-=' + ah + 'px'
-        }, at, function() {
-            nextImage();
-        });
-    }
-    function nextImage() {
-        ino++;
-        if (ino == (il + 1)) {
-            ino = 1;
-        }
-        var iw = $('#teaserimg0' + ino).width();
-        var ih = $('#teaserimg0' + ino).height();
-        $('#teaserimg0' + ino).css({
-            'left': '0px',
-            'top': '0px'
-        });
-        var aw = iw - stw;
-        var ah = ih - sth;
-        $('#tit').html($('.teaserimg0' + ino).html()).css({
-            'top': ytit + 'px'
-        }).fadeIn('fast').animate({
-            'top': '+=' + oytit + 'px'
-        }, (at - ottit)).fadeOut('slow');
-        $('#teaserimg0' + ino).fadeIn(500).animate({
-            'left': '-=' + aw + 'px',
-            'top': '-=' + ah + 'px'
-        }, at, function() {
-            if (ino > 1) {
-                $(this).delay(600).fadeOut();
+$(document).ready(function () {
+
+    let pic1 = $('#teaserimg01');
+    let pic2 = $('#teaserimg02');
+    let pic3 = $('#teaserimg03');
+    let picArr = [pic1, pic2, pic3];
+
+
+    let i = 0;
+    function changePicInit () {
+
+        for (let k = 0; k < picArr.length; k++) {
+            if (i != k) {
+                $(picArr[k]).fadeOut(2500);
+                console.log(picArr[k]);
+            } else {
+                $(picArr[k]).fadeIn(2500);
+                console.log('noup');
+                console.log(picArr[k]);
             }
-            nextImage();
-        });
+        }
+        console.log(i);
+
+        if (i == 2) {
+            i = 0;
+        } else {
+            i++;
+        }
+        setTimeout(changePicInit, 4000);
     }
-    fontSize("#ftsizer", "#inner", 11, 11, 20);
-});
-$(function() {
-    if (c) {} else {}
-    $('.print a').click(function() {
-        window.print();
-        return false;
-    });
+
+    changePicInit();
+
+
+
+
+
+
+
+
+
+
+
+
+    // var ino;
+    // var stw;
+    // var sth;
+    // var ytit;
+    // var oytit;
+    // var ottit;
+    // var at;
+    // function initTeaser() {
+    //     var iw = $('#teaserimg0' + ino).width();
+    //     var ih = $('#teaserimg0' + ino).height();
+    //     var aw = iw - stw;
+    //     var ah = ih - sth;
+    //     $('<span id="tit">' + $('.teaserimg0' + ino).html() + '</span>').appendTo('#startteaser').css({
+    //         'top': ytit + 'px'
+    //     }).fadeIn('fast').animate({
+    //         'top': '+=' + oytit + 'px'
+    //     }, (at - ottit)).fadeOut('slow');
+    //     $('#teaserimg0' + ino).animate({
+    //         'left': '-=' + aw + 'px',
+    //         'top': '-=' + ah + 'px'
+    //     }, at, function () {
+    //         nextImage();
+    //     });
+    // }
+
+    // function nextImage() {
+    //     ino++;
+    //     if (ino == (il + 1)) {
+    //         ino = 1;
+    //     }
+    //     var iw = $('#teaserimg0' + ino).width();
+    //     var ih = $('#teaserimg0' + ino).height();
+    //     $('#teaserimg0' + ino).css({
+    //         'left': '0px',
+    //         'top': '0px'
+    //     });
+    //     var aw = iw - stw;
+    //     var ah = ih - sth;
+    //     $('#tit').html($('.teaserimg0' + ino).html()).css({
+    //         'top': ytit + 'px'
+    //     }).fadeIn('fast').animate({
+    //         'top': '+=' + oytit + 'px'
+    //     }, (at - ottit)).fadeOut('slow');
+    //     $('#teaserimg0' + ino).fadeIn(500).animate({
+    //         'left': '-=' + aw + 'px',
+    //         'top': '-=' + ah + 'px'
+    //     }, at, function () {
+    //         if (ino > 1) {
+    //             $(this).delay(600).fadeOut();
+    //         }
+    //         nextImage();
+    //     });
+    // }
+
+    // initTeaser();
+
+
+
 });
